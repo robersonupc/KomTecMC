da7e79134601a4ddfcfdfd6acdf0ba41b1e68965
Roberson Santos <robersonupc@gmail.com>
2018-03-11 00:27:51 -0300

    Projeto criado

---
+++ src/main/java/br/com/rdssystemas/komtecmc/KomTecMcApplication.java
@@ -0,0 +1,12 @@
+package br.com.rdssystemas.komtecmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomTecMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomTecMcApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/rdssystemas/komtecmc/KomTecMcApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.rdssystemas.komtecmc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KomTecMcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
