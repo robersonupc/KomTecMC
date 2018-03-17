4c66ba977d000e737fb39586467c94fa82e77051
Roberson Santos <robersonupc@gmail.com>
2018-03-17 18:02:08 -0300

    Estado e Cidade

--- src/main/java/br/com/rdssystemas/komtecmc/KomTecMcApplication.java
+++ src/main/java/br/com/rdssystemas/komtecmc/KomTecMcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.rdssystemas.komtecmc.domain.Categoria;
+import br.com.rdssystemas.komtecmc.domain.Cidade;
+import br.com.rdssystemas.komtecmc.domain.Estado;
 import br.com.rdssystemas.komtecmc.domain.Produto;
 import br.com.rdssystemas.komtecmc.repositories.CategoriaRepository;
+import br.com.rdssystemas.komtecmc.repositories.CidadeRepository;
+import br.com.rdssystemas.komtecmc.repositories.EstadoRepository;
 import br.com.rdssystemas.komtecmc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class KomTecMcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KomTecMcApplication.class, args);
@@ -42,8 +52,27 @@ public class KomTecMcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Goiás", "GO");
+		Estado est2 = new Estado(null, "Mato Grosso", "MT");
+		Estado est3 = new Estado(null, "Minas Gerais", "MG");
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2,p3));
+		
+		Cidade cid1 = new Cidade(null, "Goiânia", est1);
+		Cidade cid2 = new Cidade(null, "Aparecida de Goiânia", est1);
+		Cidade cid3 = new Cidade(null, "Anapolis", est1);
+		Cidade cid4 = new Cidade(null, "Cuiabá", est2);
+		Cidade cid5 = new Cidade(null, "Rondonópolis", est2);
+		Cidade cid6 = new Cidade(null, "Belo Horizonte", est3);
+		Cidade cid7 = new Cidade(null, "Uberlândia", est3);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1, cid2, cid3));
+		est2.getCidades().addAll(Arrays.asList(cid4, cid5));
+		est3.getCidades().addAll(Arrays.asList(cid6, cid7));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2,est3));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2,cid3, cid4, cid5, cid6, cid7));
 		
 	}
 	
---
+++ src/main/java/br/com/rdssystemas/komtecmc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package br.com.rdssystemas.komtecmc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.rdssystemas.komtecmc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
