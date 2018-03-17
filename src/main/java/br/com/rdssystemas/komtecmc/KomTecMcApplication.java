87b9b8e3739b5af6eb84f4a622503dd6ccf7a160
Roberson Santos <robersonupc@gmail.com>
2018-03-17 11:52:21 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/rdssystemas/komtecmc/KomTecMcApplication.java
+++ src/main/java/br/com/rdssystemas/komtecmc/KomTecMcApplication.java
@@ -1,12 +1,33 @@
 package br.com.rdssystemas.komtecmc;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.rdssystemas.komtecmc.domain.Categoria;
+import br.com.rdssystemas.komtecmc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class KomTecMcApplication {
+public class KomTecMcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KomTecMcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 }
