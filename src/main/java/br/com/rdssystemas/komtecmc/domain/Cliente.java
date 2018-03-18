45f0cb50f90c686820851fb8913ce184e62072cd
Roberson Santos <robersonupc@gmail.com>
2018-03-18 06:50:07 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/br/com/rdssystemas/komtecmc/domain/Cliente.java
+++ src/main/java/br/com/rdssystemas/komtecmc/domain/Cliente.java
@@ -14,6 +14,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import br.com.rdssystemas.komtecmc.domain.enums.TipoCliente;
 
 @Entity
@@ -29,6 +31,7 @@ public class Cliente implements Serializable {
 	private String cpfCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
