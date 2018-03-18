0c888857dcc0a0062040e1e53a126d2b096db161
Roberson Santos <robersonupc@gmail.com>
2018-03-18 18:34:55 -0300

    Inserindo uma nova Categoria com Post

--- src/main/java/br/com/rdssystemas/komtecmc/resources/CategoriaResource.java
+++ src/main/java/br/com/rdssystemas/komtecmc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package br.com.rdssystemas.komtecmc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.rdssystemas.komtecmc.domain.Categoria;
 import br.com.rdssystemas.komtecmc.services.CategoriaService;
@@ -22,6 +26,20 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		
+		 obj = service.insert(obj);
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				 .path("/{id}").buildAndExpand(obj.getId()).toUri();
+		 
+		 return ResponseEntity.created(uri).build();
+		 
 	}
+	
 }
+
+
 
